1dbfa0d857b8651be96b29aa1b0bf4c6ad171d33
kooszabi <205829315+kooszabi@users.noreply.github.com>
2026-07-31 22:58:27 +0200

    best and worst topics

--- Frontend/src/Pages/Statistics.jsx
+++ Frontend/src/Pages/Statistics.jsx
@@ -2,6 +2,7 @@ import './Statistics.css';
 import { useEffect, useState } from 'react';
 import api from "../api/axios";
 import { LessonProgressCard } from './Components/LessonProgressCard';
+import { LessonProgressBar } from './Components/LessonProgressBar';
 
 export function Statistics() {
     // username
@@ -22,7 +23,13 @@ export function Statistics() {
     const [completedCodingExercises, setCompletedCodingExercises] = useState(0);
     const [codingExercisesProgress, setCodingExercisesProgress] = useState(0);
     const [remainingCodingExercises,setRemainingCodingExercises] = useState(0);
-    //const []
+    // topics progress list
+    const [topicProgressList, setTopicProgressList] = useState([]);
+    // best topic result
+    const [bestTopicResult, setBestTopicResult] = useState([]);
+    // worst topic result
+    const [worstTopicResult, setWorstTopicResult] = useState([]);
+
     useEffect(() => {
         api.get(
             "http://localhost:5000/api/statistics/get-statistics"
@@ -45,9 +52,42 @@ export function Statistics() {
             setAllCodingExercises(res.data.overall_statistics.all_coding_exercises)
             setRemainingCodingExercises(res.data.overall_statistics.remaining_coding_exercises)
             setCodingExercisesProgress(res.data.overall_statistics.coding_exercise_progress)
+            // topics progress list for the progress bar
+            setTopicProgressList(res.data.topics_progress_list)
+            console.log("topics_progress_list", res.data.topics_progress_list)
+            console.log("topics_progress_list_id", res.data.topics_progress_list[0].topic_id)
+            // best topic result-
+            setBestTopicResult(res.data.best_topic_result);
+            console.log("Best topic result:", res.data.best_topic_result)>
+            // worst topic result
+            setWorstTopicResult(res.data.wort_topic_result);
+            console.log("Worst topic result: ", res.data.wort_topic_result);
         })
     }, [])
 
+    function averageBestWidth() {
+        if (bestTopicResult.length === 0) {
+            return 0
+        }
+        let sumOfTheScores = 0;
+        bestTopicResult.map(score => {
+            sumOfTheScores += score.topic_result;
+        })
+        return sumOfTheScores / bestTopicResult.length;
+    }
+
+    function averageWorstWidth() {
+        if (worstTopicResult.length === 0) {
+            return 0
+        }
+        let sumOfTheScores = 0;
+        worstTopicResult.map(score => {
+            sumOfTheScores += score.topic_result;
+        })
+        return sumOfTheScores / worstTopicResult.length;
+    }
+
+    if (!topicProgressList) return <p>Loading the statistics...</p>
     return (
         <div className='statistics-container'>
             <h1>Welcome back, {username}</h1>
@@ -79,6 +119,136 @@ export function Statistics() {
                 />
 
             </div>
+
+            <div className='topic-specific-statistics-title-container'>
+                <div className='topic-specific-statistics-hline' />
+                <span className='topic-specific-statistics-title'>
+                    Topics Progresses
+                </span>
+                <div className='topic-specific-statistics-hline' />
+            </div>
+
+            <div className='topic-specific-statistics'>
+
+                {topicProgressList.map(topic => {
+                    return (
+                        <LessonProgressBar 
+                            allTopicLesson={topic?.all_topic_lesson}
+                            completedTopicLesson={topic?.completed_topic_lesson}
+                            topicName={topic?.topic_name}
+                            topicProgress={topic?.topic_progress}
+                            topicId={topic?.topic_id}
+                        />
+                    )
+                })}
+
+            </div>
+
+            <div className='best-and-worst-topics'>
+
+                <div className='best-topic-container'>
+                    
+                    <div className='best-topic-information-container'>
+                        <div className='best-topic-information-image-container'>
+                            <div className='best-topic-image-div'>
+                                <img className='best-topic-information-image' src='/images/statistics/icons8-trophy-100.png' />
+                            </div>
+                        </div>
+
+                        <div className='best-topic-information-div'>
+                            <div className='best-topic-sign-container'>
+                                <img className='best-topic-sign-image' src='/images/statistics/icons8-up-right-100.png' />
+                                <span className='best-topic-sign'>
+                                    {bestTopicResult.length > 1 ? "BEST TOPICS" : "BEST TOPIC"}
+                                </span>
+                            </div>
+
+                            <span className='best-topic-title'>
+                                {bestTopicResult.length > 1 ? "Best Topics" : "Best Topic"}
+                            </span>
+
+                            {bestTopicResult.map(best => {
+                                return (
+                                    <span className='best-topic-name'>
+                                        {best.topic_name}
+                                    </span>
+                                )
+                            })}
+
+                            {bestTopicResult.map(best => {
+                                return (
+                                    <span className='best-topic-result'>
+                                        {best.topic_result}%
+                                    </span>
+                                )
+                            })}
+
+                        </div>
+
+                    </div>
+
+                    <div className='best-topic-progress-bar-container'>
+
+                        <div className='best-topic-progress'>
+                            <div className='best-topic-progress-fill' style={{width: `${averageBestWidth()}%`}} />
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div className='best-topic-container'>
+                    
+                    <div className='best-topic-information-container'>
+                        <div className='best-topic-information-image-container'>
+                            <div className='worst-topic-image-div'>
+                                <img className='best-topic-information-image' src='/images/statistics/icons8-decrease-100.png' />
+                            </div>
+                        </div>
+
+                        <div className='best-topic-information-div'>
+                            <div className='worst-topic-sign-container'>
+                                <img className='best-topic-sign-image' src='/images/statistics/icons8-down-right-100.png' />
+                                <span className='worst-topic-sign'>
+                                    {worstTopicResult.length > 1 ? "WORST TOPICS" : "WORST TOPIC"}
+                                </span>
+                            </div>
+
+                            <span className='best-topic-title'>
+                                {worstTopicResult.length > 1 ? "Worst Topics" : "Worst Topic"}
+                            </span>
+
+                            {worstTopicResult.map(worst => {
+                                return (
+                                    <span className='best-topic-name'>
+                                        {worst.topic_name}
+                                    </span>
+                                )
+                            })}
+
+                            {worstTopicResult.map(worst => {
+                                return (
+                                    <span className='worst-topic-result'>
+                                        {worst.topic_result}%
+                                    </span>
+                                )
+                            })}
+
+                        </div>
+
+                    </div>
+
+                    <div className='best-topic-progress-bar-container'>
+
+                        <div className='best-topic-progress'>
+                            <div className='worst-topic-progress-fill' style={{width: `${averageWorstWidth()}%`}} />
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+
         </div>
     )
 }
